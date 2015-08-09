21e04666f213689771be9cf824636b54fcc52a14
Michael Steinberg <mst@tu-clausthal.de>
2015-08-09 20:21:28 +0200

    added c++ style circular buffer (fifo)

--- microlib/circular_buffer.hpp
+++ microlib/circular_buffer.hpp
@@ -107,6 +107,81 @@ namespace util {
 		size_t front_;
 	};
 
+	template< typename T, size_t Size >
+	class circular_buffer2 {
+	public:
+		circular_buffer2()
+			: front_(0), back_(0)
+		{}
+
+		size_t size() const {
+			return back_ - front_;
+		}
+
+		bool push(T elem) {
+			if (size() != Size) {
+				new (ptr(back_)) T(std::move(elem));
+				++back_;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		template< typename... Args >
+		bool emplace( Args&&... args )
+		{
+			if (size() != Size) {
+				new (ptr(back_)) T(std::forward<Args>(args)...);
+				++back_;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		T& front()
+		{
+			return *ptr(front_);
+		}
+
+		const T& front() const
+		{
+			return *ptr(front_);
+		}
+
+		void pop()
+		{
+			ptr(front_)->~T();
+			++front_;
+		}
+
+		void clear() {
+			while (size()) {
+				pop();
+			}
+		}
+
+		~circular_buffer2()
+		{
+			clear();
+		}
+
+	private:
+		
+		T* ptr(size_t index) {
+			return reinterpret_cast<T*>(&data_[index%Size]);
+		}
+
+		const T* ptr(size_t index) const {
+			return reinterpret_cast<const T*>(&data_[index%Size]);
+		}
+
+		using storage_type = typename std::aligned_storage<sizeof(T), alignof(T)>::type;
+		storage_type data_[Size];
+		size_t front_;
+		size_t back_;
+	};
 
 }
 
