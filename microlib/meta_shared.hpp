ec816e7783dbb124fe538795fd58bc7514518049
Michael Steinberg <michsteinb@gmail.com>
2020-01-18 23:12:27 +0100

    split meta type library up into its components

---
+++ microlib/meta_shared.hpp
@@ -0,0 +1,20 @@
+/*
+ * meta_shared.hpp
+ *
+ *  Created on: 21.10.2016
+ *      Author: Michael
+ */
+
+#ifndef MICROLIB_META_SHARED_HPP_
+#define MICROLIB_META_SHARED_HPP_
+
+namespace ulib {
+namespace meta {
+
+	template< typename List, size_t Index >
+	struct get;
+
+}
+}
+
+#endif
